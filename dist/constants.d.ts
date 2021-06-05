c67c8a66f10c12a09410467ee77fd80157f1d004
Christian Koop <contact@sprax2013.de>
2021-06-05 11:16:58 +0200

    Release v3.0.0-SNAPSHOT

---
+++ dist/constants.d.ts
@@ -0,0 +1,4 @@
+export declare const regular: string[];
+export declare const normal: {
+    [key: string]: string | null;
+};
---
+++ dist/constants.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.normal = exports.regular = void 0;
+exports.regular = [
+    'art-lojban',
+    'cel-gaulish',
+    'no-bok',
+    'no-nyn',
+    'zh-guoyu',
+    'zh-hakka',
+    'zh-min',
+    'zh-min-nan',
+    'zh-xiang'
+];
+exports.normal = {
+    'en-gb-oed': 'en-GB-oxendict',
+    'i-ami': 'ami',
+    'i-bnn': 'bnn',
+    'i-default': null,
+    'i-enochian': null,
+    'i-hak': 'hak',
+    'i-klingon': 'tlh',
+    'i-lux': 'lb',
+    'i-mingo': null,
+    'i-navajo': 'nv',
+    'i-pwn': 'pwn',
+    'i-tao': 'tao',
+    'i-tay': 'tay',
+    'i-tsu': 'tsu',
+    'sgn-be-fr': 'sfb',
+    'sgn-be-nl': 'vgt',
+    'sgn-ch-de': 'sgg',
+    'art-lojban': 'jbo',
+    'cel-gaulish': null,
+    'no-bok': 'nb',
+    'no-nyn': 'nn',
+    'zh-guoyu': 'cmn',
+    'zh-hakka': 'hak',
+    'zh-min': null,
+    'zh-min-nan': 'nan',
+    'zh-xiang': 'hsn'
+};
+//# sourceMappingURL=constants.js.map
